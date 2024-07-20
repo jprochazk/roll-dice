dc4c9f9bacf96f8edbb874b0f98cc1b76dac0ae1
jprochazk <1665677+jprochazk@users.noreply.github.com>
2024-07-20 14:19:06 +0200

    errors

--- src/error.rs
+++ src/error.rs
@@ -19,3 +19,41 @@ impl std::fmt::Display for Error {
     write!(f, "error: {}", self.message)
   }
 }
+
+impl Error {
+  pub fn overflow() -> Self {
+    Self::new("integer overflow")
+  }
+
+  pub fn divide_by_zero() -> Self {
+    Self::new("can't divide by zero")
+  }
+
+  pub fn roll_min_times() -> Self {
+    Self::new("can't roll less than 0 times")
+  }
+
+  pub fn roll_min_sides() -> Self {
+    Self::new("can't roll a less than 0-sided die")
+  }
+
+  pub fn too_many_rolls() -> Self {
+    Self::new("too many rolls")
+  }
+
+  pub fn unexpected_eof() -> Self {
+    Self::new("unexpected end of input")
+  }
+
+  pub fn unexpected_input(input: &str) -> Self {
+    Self::new(format!("unexpected input: {input:?}"))
+  }
+
+  pub fn expected_input(expected: &str, got: &str) -> Self {
+    Self::new(format!("expected {expected:?}, got {got:?}"))
+  }
+
+  pub fn invalid_integer(v: &str, e: impl std::fmt::Display) -> Self {
+    Self::new(format!("invalid integer {v:?}: {e}"))
+  }
+}
